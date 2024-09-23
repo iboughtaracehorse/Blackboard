65ab22902fbb247e2721a7d9992cbe2b01438c71
Dariia Dushynska <ddushynska@kse.org.ua>
2024-09-23 13:40:19 +0300

    initial code updated

--- BlackboardVS.cpp
+++ BlackboardVS.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 const int BOARD_WIDTH = 80;
 const int BOARD_HEIGHT = 25;
+
+class Figure {
+	
+};
 
 struct Board {
 	std::vector<std::vector<char>> grid;
@@ -16,21 +26,34 @@ struct Board {
 		}
 	}
 	void drawTriangle(int x, int y, int height) {
+		if (height <= 0) return; 
 		for (int i = 0; i < height; ++i) {
-			int numStars = 2 * i + 1;
-			int leftMost = x - i;
-			for (int j = 0; j < numStars; ++j) {
-				int position = leftMost + j;
-				if (position >= 0 && position < BOARD_WIDTH && (y + i) <
-					BOARD_HEIGHT && (y + i) >= 0)
-					grid[y + i][position] = '*';
+			int leftMost = x - i; 
+			int rightMost = x + i; 
+			int posY = y + i; 
+			if (posY < BOARD_HEIGHT) {
+				if (leftMost >= 0 && leftMost < BOARD_WIDTH) 
+					grid[posY][leftMost] = '*';
+				if (rightMost >= 0 && rightMost < BOARD_WIDTH && leftMost != rightMost)
+					grid[posY][rightMost] = '*';
 			}
+		}
+		for (int j = 0; j < 2 * height - 1; ++j) {
+			int baseX = x - height + 1 + j;
+			int baseY = y + height - 1;
+			if (baseX >= 0 && baseX < BOARD_WIDTH && baseY < BOARD_HEIGHT) 
+				grid[baseY][baseX] = '*';
 		}
 	}
 };
+
 int main() {
 	Board board;
-	board.drawTriangle(80, 1, 5);
+	board.drawTriangle(10, 1, 5);
+	board.drawTriangle(13, 1, 7);
 	board.print();
 	return 0;
-}
+};
+
+
+
