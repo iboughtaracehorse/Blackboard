2b4ea0ea03e14af30d674e84ac22e2eccd70acfc
Dariia Dushynska <ddushynska@kse.org.ua>
2024-09-23 14:54:12 +0300

    Square and Triangle classes added. Triangle class functionality added. getGrid() method added to Board class

--- BlackboardVS.cpp
+++ BlackboardVS.cpp
@@ -10,58 +10,79 @@ using namespace std;
 const int BOARD_WIDTH = 80;
 const int BOARD_HEIGHT = 25;
 
-class Figure {
-
-protected: 
-	int X;
-	int Y;
-	int HEIGHT;
-
-public:
-	Figure(int x, int y, int height) { X = x; Y = y; HEIGHT = height; }
-
-};
-
 struct Board {
 
-	std::vector<std::vector<char>> grid;
+	vector<vector<char>> grid;
 
-	Board() : grid(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH, '-')) {}
+	Board() : grid(BOARD_HEIGHT, vector<char>(BOARD_WIDTH, '-')) {}
 
 	void print() {
 		for (auto& row : grid) {
 			for (char c : row) {
-				std::cout << c;
+				cout << c;
 			}
-			std::cout << "\n";
+			cout << "\n";
 		}
 	}
 
-	void drawTriangle(int x, int y, int height) {
-		if (height <= 0) return; 
+	vector<vector<char>>& getGrid() {
+		return grid;
+	}
+};
+
+class Figure {
+
+protected: 
+	int x;
+	int y;
+	int height;
+	vector<vector<char>>* grid;
+
+public:
+
+	Figure(Board& board) { x = 0; y = 0; height = 0; grid = &board.getGrid(); }
+
+};
+
+class Square: public Figure {
+
+public:
+	Square(Board& board) : Figure(board) {};
+};
+
+class Triangle: public Figure {
+
+public:
+
+	Triangle(Board& board) : Figure(board) {};
+
+	void draw() {
+		if (height <= 0) return;
 		for (int i = 0; i < height; ++i) {
-			int leftMost = x - i; 
-			int rightMost = x + i; 
-			int posY = y + i; 
+			int leftMost = x - i;
+			int rightMost = x + i;
+			int posY = y + i;
 			if (posY < BOARD_HEIGHT) {
-				if (leftMost >= 0 && leftMost < BOARD_WIDTH) 
-					grid[posY][leftMost] = '*';
+				if (leftMost >= 0 && leftMost < BOARD_WIDTH)
+					(*grid)[posY][leftMost] = '*';
 				if (rightMost >= 0 && rightMost < BOARD_WIDTH && leftMost != rightMost)
-					grid[posY][rightMost] = '*';
+					(*grid)[posY][rightMost] = '*';
 			}
 		}
 		for (int j = 0; j < 2 * height - 1; ++j) {
 			int baseX = x - height + 1 + j;
 			int baseY = y + height - 1;
-			if (baseX >= 0 && baseX < BOARD_WIDTH && baseY < BOARD_HEIGHT) 
-				grid[baseY][baseX] = '*';
+			if (baseX >= 0 && baseX < BOARD_WIDTH && baseY < BOARD_HEIGHT)
+				(*grid)[baseY][baseX] = '*';
 		}
 	}
 };
 
+
+
 int main() {
 	Board board;
-	board.drawTriangle(10, 1, 5);
+	
 	board.print();
 	return 0;
 };
